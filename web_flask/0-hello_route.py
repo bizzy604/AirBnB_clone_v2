8dacd0beca5cc8c8b9879820770113aee48b97f2
bizzy604 <kevinyamatta50@gmail.com>
2024-07-26 02:00:10 -0700

    Automated commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Disaplay 'Hello HBNB' on the root route """
-    return "Hello HBNB!"
+    return ("Hello HBNB!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,12 +12,12 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Display 'Hello HBNB!' on the root route."""
-    return "Hello HBNB!"
+    return ("Hello HBNB!")
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Display 'HBNB' on the /hbnb route."""
-    return "HBNB"
+    return ("HBNB")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
